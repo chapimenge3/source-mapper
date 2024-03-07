8930ff1e665b4896a3b17e51e6d776638b0be62e
Chapi Menge <chapimenge3@gmail.com>
2024-03-07 08:55:28 +0300

    add: generating from source map content

--- app/page.js
+++ app/page.js
@@ -16,6 +16,40 @@ export default function Home() {
   const [input, setInput] = useState('')
   const [loading, setLoading] = useState(false)
 
+  const parseFilesFromSourceMap = (sourceMaps) => {
+    const files = []
+    // sourceMaps.forEach((sourceMap) => {
+    //   const content = JSON.parse(sourceMap)
+    //   const sources = content.sources
+    //   const sourcesContent = content.sourcesContent
+
+    //   sources.forEach((source, index) => {
+    //     // ignores node_modules or webpack
+    //     if (source.includes('node_modules') || source.includes('webpack')) return
+
+    //     // remove all ../ from the source
+    //     var fileName = source.replace(/\.\.\//g, '')
+    //     files.push({ file: fileName, content: sourcesContent[index] })
+    //   })
+    // })
+    for (let i = 0; i < sourceMaps.length; i++) {
+      const content = JSON.parse(sourceMaps[i])
+      const sources = content.sources
+      const sourcesContent = content.sourcesContent
+
+      for (let j = 0; j < sources.length; j++) {
+        // ignores node_modules or webpack
+        if (sources[j].includes('node_modules') || sources[j].includes('webpack')) continue
+
+        // remove all ../ from the source
+        var fileName = sources[j].replace(/\.\.\//g, '')
+        files.push({ file: fileName, content: sourcesContent[j] })
+      }
+    }
+
+    return files
+  }
+
   async function generateFileFromSourceMap(urls) {
     const sourceCode = await Promise.all(urls.map(async (url) => {
       const content = JSON.parse(await getWebPageContent(url))
@@ -24,32 +58,45 @@ export default function Home() {
 
     // this will contains the source code and the file name
     // {file: 'File name', content: 'File content'}
-    const files = {}
+    const files = parseFilesFromSourceMap(sourceCode)
 
-    sourceCode.forEach((content, index) => {
-      // check if the content is a source map
-      if (!content.sources) return
+    // sourceCode.forEach((content, index) => {
+    //   // check if the content is a source map
+    //   if (!content.sources) return
 
-      // content is mapping file which contains
-      // sources -> array of source files
-      // sourcesContent -> array of source files content
-      const sources = content.sources
-      const sourcesContent = content.sourcesContent
+    //   // content is mapping file which contains
+    //   // sources -> array of source files
+    //   // sourcesContent -> array of source files content
+    //   const sources = content.sources
+    //   const sourcesContent = content.sourcesContent
 
-      sources.forEach((source, index) => {
-        // ignores node_modules or webpac
-        if (source.includes('node_modules') || source.includes('webpack')) return
+    //   sources.forEach((source, index) => {
+    //     // ignores node_modules or webpac
+    //     if (source.includes('node_modules') || source.includes('webpack')) return
 
-        // remove all ../ from the source
-        var fileName = source.replace(/\.\.\//g, '')
-        files[fileName] = sourcesContent[index]
-      })
-    })
+    //     // remove all ../ from the source
+    //     var fileName = source.replace(/\.\.\//g, '')
+    //     // files[fileName] = sourcesContent[index]
+    //     files.push({ file: fileName, content: sourcesContent[index] })
+    //   })
+    // })
+
 
     // log all file names
     console.log(`Got ${Object.keys(files).length} files`)
 
     return files
+  }
+
+  const isMappingFile = (content) => {
+    try {
+      JSON.parse(content)
+      return true
+    }
+    catch (e) {
+      console.error('Error parsing content', e)
+      return false
+    }
   }
 
   const handleGenerateSourceCode = async () => {
@@ -64,23 +111,48 @@ export default function Home() {
     }
     setLoading(true)
     console.log('Generating source code')
-    const allSourceMaps = await getAllSourceMaps(input)
-    toast({
-      title: "Source Code",
-      description: `Found ${allSourceMaps.length} source maps`,
-    })
-    console.log('All source maps', allSourceMaps)
-    const files = await generateFileFromSourceMap(allSourceMaps)
-    toast({
-      title: "Source Code",
-      description: `Generated ${Object.keys(files).length} files`,
-    })
+    var files = []
+    try {
+      console.log('isMappingFile', isMappingFile(input))
+      if (!isMappingFile(input)) {
+
+        const allSourceMaps = await getAllSourceMaps(input)
+        toast({
+          title: "Source Code",
+          description: `Found ${allSourceMaps.length} source maps`,
+        })
+        console.log('All source maps', allSourceMaps)
+        files = await generateFileFromSourceMap(allSourceMaps)
+        toast({
+          title: "Source Code",
+          description: `Generated ${Object.keys(files).length} files`,
+        })
+      } else if (isMappingFile(input)) {
+        files = parseFilesFromSourceMap([input])
+        toast({
+          title: "Source Code",
+          description: `Generated ${Object.keys(files).length} files`,
+        })
+      }
+    } catch (e) {
+      console.error('Error generating source code', e)
+      toast({
+        title: "Error",
+        description: "Error generating source code",
+        variant: "destructive"
+      })
+      setLoading(false)
+      return
+    }
 
     // Generate a zip file that contains all the source code and file name
     const zip = new JSZip();
-    Object.keys(files).forEach((fileName) => {
-      zip.file(fileName, files[fileName]);
-    });
+    // Object.keys(files).forEach((fileName) => {
+    //   zip.file(fileName, files[fileName]);
+    // });
+    files.forEach((file) => {
+      zip.file(file.file, file.content)
+    })
 
     zip.generateAsync({ type: "blob" })
       .then(function (content) {
